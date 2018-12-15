a0f778c5592cecaeb46656a196d3699dd6e15fee
jipei <962319214@qq.com>
2018-12-15 14:41:45 +0800

    第五天

---
+++ pinyougou/pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.pinyougou.shop.controller;
+
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/login")
+@RestController
+public class LoginController {
+
+    @GetMapping("/getUsername")
+    public Map<String,String> getUsername(){
+        Map<String,String> map=new HashMap<>();
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        map.put("username",username);
+        return map;
+    }
+}
